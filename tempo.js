2cce05842c6d26132d5e2cef9d375b84cf11a5ee
Barney He <barneyhe@outlook.com>
2024-12-18 22:20:23 +0800

    create tempo.js

---
+++ tempo.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http://tampermonkey.net/
+// @version      2024-12-18
+// @description  try to take over the world!
+// @author       BarneyHe
+// @match        https://www.bilibili.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("hello world");
+    // Your code here...
+})();
