b90e5e48bd595fd9f12792f3e3432608e6b656b6
Barney He <barneyhe@outlook.com>
2024-12-18 22:37:24 +0800

    next version

--- tempo.js
+++ tempo.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         hello world
 // @namespace    http://tampermonkey.net/
-// @version      2024-12-18
-// @description  try to take over the world!
+// @version      2024-12-19
+// @description  hello world and say hi
 // @author       BarneyHe
 // @match        https://www.bilibili.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
@@ -11,6 +11,6 @@
 
 (function() {
     'use strict';
-    console.log("hello world");
+    console.log("hello world and say hi");
     // Your code here...
 })();
